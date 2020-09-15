ac95d862cea926d410b1da4820b62eaee0c2f3fc
oherbert <o_herbert@hotmail.com>
2020-09-15 18:44:22 -0300

    Primeira versão

---
+++ backend/src/test/java/com/pesquisa/PesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
